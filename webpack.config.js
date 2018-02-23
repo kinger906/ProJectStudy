0c247c939effceeea9eeee34c3b9a5efadf5529d
kingtiger <605970498@qq.com>
2018-02-23 22:33:01 +0800

    webpack打包学习

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,8 @@ module.exports = {
     entry:{
         'build-eg1':'./site/js/js-eg1/template.js',
         'build-eg2':'./site/js/js-eg2/app.js',
-        'build-eg3':'./site/js/js-eg3/app.js'
+        'build-eg3':'./site/js/js-eg3/app.js',
+        'build-eg4':'./site/js/js-eg4/app.js'
     },
     output:{
         filename:'./site/build/[name].js'
@@ -12,6 +13,11 @@ module.exports = {
             {
                 test: /\.css$/,
                 use: ['style-loader', 'css-loader']
+            },
+            {
+                test:/.js$/,
+                loaders:["babel-loader"],
+                exclude:"/node_module/"
             }
         ]
     }
